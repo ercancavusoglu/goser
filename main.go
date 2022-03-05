b6a58dc26a3ed314adafa14bf4a987fa8dffa8a5
ercan.cavusoglu <ercan.cavusoglu@enuygun.com>
2022-03-05 19:30:11 +0300

    routes

--- main.go
+++ main.go
@@ -1,34 +1,41 @@
 package main
 
 import (
-	"fmt"
-	"github.com/ercancavusoglu/goser/database"
-	"github.com/gofiber/fiber/v2"
 	"log"
+
+	"github.com/ercancavusoglu/goser/database"
+	"github.com/ercancavusoglu/goser/routes"
+	"github.com/gofiber/fiber/v2"
 )
 
-type Todo struct {
-	Id        int    `json:"id"`
-	Name      string `json:"name"`
-	Completed bool   `json:"completed"`
-}
-
-var todos = []Todo{
-	{Id: 1, Name: "Walk", Completed: false},
-	{Id: 2, Name: "Sleep", Completed: false},
-}
-
 func welcome(c *fiber.Ctx) error {
-	fmt.Println("🥉 Son handler")
-	return c.SendString("Merhaba, Dünya 👋!")
+	return c.SendString("Welcome to an Awesome API")
+}
+
+func setupRoutes(app *fiber.App) {
+	// Welcome endpoint
+	app.Get("/api", welcome)
+	// User endpoints
+	app.Post("/api/users", routes.CreateUser)
+	app.Get("/api/users", routes.GetUsers)
+	app.Get("/api/users/:id", routes.GetUser)
+	app.Delete("/api/users/:id", routes.DeleteUser)
+	// Product endpoints
+	app.Post("/api/products", routes.CreateProduct)
+	app.Get("/api/products", routes.GetProducts)
+	app.Get("/api/products/:id", routes.GetProduct)
+	app.Put("/api/products/:id", routes.UpdateProduct)
+	// Order endpoints
+	app.Post("/api/orders", routes.CreateOrder)
+	app.Get("/api/orders", routes.GetOrders)
+	app.Get("/api/orders/:id", routes.GetOrder)
 }
 
 func main() {
 	database.ConnectDb()
 
 	app := fiber.New()
-
-	app.Get("/api", welcome)
+	setupRoutes(app)
 
 	log.Fatal(app.Listen(":3000"))
 
