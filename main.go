946f1370a0231a80ddbb785f7c2749ec196e9840
ercan.cavusoglu <ercan.cavusoglu@enuygun.com>
2022-03-05 19:10:10 +0300

    init

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"./database"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+type Todo struct {
+	Id        int    `json:"id"`
+	Name      string `json:"name"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []Todo{
+	{Id: 1, Name: "Walk", Completed: false},
+	{Id: 2, Name: "Sleep", Completed: false},
+}
+
+func welcome(c *fiber.Ctx) error {
+	fmt.Println("🥉 Son handler")
+	return c.SendString("Merhaba, Dünya 👋!")
+}
+
+func main() {
+	database.ConnectDb()
+
+	app := fiber.New()
+
+	app.Get("/api", welcome)
+
+	log.Fatal(app.Listen(":3000"))
+
+}
